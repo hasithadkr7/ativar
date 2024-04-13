6b2a7eceaa2f3866a440d0f08b7c9c55dced232e
Zoey de Souza Pessanha <zoey.spessanha@zeetech.io>
2024-04-13 18:13:53 -0300

    fix nix

--- flake.nix
+++ flake.nix
@@ -13,25 +13,16 @@
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = import inputs.systems;
       perSystem = {pkgs, ...}: let
-        inherit (pkgs) callPackage;
         inherit (pkgs.beam) packagesWith;
-        inherit (pkgs.beam.packages) erlangR26;
+        inherit (pkgs.beam.interpreters) erlangR26;
         erl = packagesWith erlangR26;
       in {
-        packages.default = let
-          node = callPackage ./assets/default.nix {};
-          inherit (node.shell) nodeDependencies;
-        in
-          erl.callPackage ./nix/ativar.nix {
-            inherit nodeDependencies;
-            inherit (pkgs) nix-gitignore;
-          };
         devShells.default = with pkgs;
           mkShell {
             name = "ativar";
             shellHook = "mkdir -p .nix-mix";
             packages = with pkgs;
-              [erlangR26.elixir_1_16 nodejs_18 postgresql node2nix]
+              [erl.elixir_1_16 nodejs_18 postgresql node2nix]
               ++ lib.optional stdenv.isLinux [inotify-tools]
               ++ lib.optional stdenv.isDarwin [
                 darwin.apple_sdk.frameworks.CoreServices
